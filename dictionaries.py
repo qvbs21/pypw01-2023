85bfb5808ecbf70edcaeb2b5e6b46546fe7cfd95
jakubjastrzebski <jakub_21@icloud.com>
2023-10-15 13:45:49 +0200

    Add commands validation

--- dictionaries.py
+++ dictionaries.py
@@ -1,6 +1,13 @@
 phones = {}
+commands = [
+    "new_contact", "show_contacts"
+]
 while True:
-    command = input("Peek an option: new_contact, show_contacts")
+    command = input(f"Peek an option: {commands} ")
+    while command not in command:
+        print("Unknown command, try again")
+        command = input(f"Peek an option: {commands} ")
+
     if command == "new_contact":
         full_name = input("Input full name: ")
         phone_num = input("Input phone number: ")
