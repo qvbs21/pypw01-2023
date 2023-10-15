ef9ecaa4f6a5eec79813d63e88615cafa2b8565c
jakubjastrzebski <jakub_21@icloud.com>
2023-10-15 10:46:01 +0200

    Refactor

--- scratch.py
+++ scratch.py
@@ -1,13 +1,8 @@
 def sweap_max(items: list) -> list:
-    max_pos = 0
-    for i in range(len(items)):
-        if items[i] >= items[max_pos]:
-            max_pos = i
-    temp = items[0]
-    items[0] = items[max_pos]
-    items[max_pos] = temp
+    max_pos = items.index(max(items))
+    items[0], items[max_pos] = items[max_pos],items[0]
     return items
 
-a = [10, 21, 19, 9, 4]
+a = [10, 21, 19, 9, 22]
 result = sweap_max(a)
 print(result)
